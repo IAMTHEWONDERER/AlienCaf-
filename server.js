db79423d0d9bc3f509bd8221ef6dea18e1defdef
eladibyoussef <eladibyoussef230@gmail.com>
2024-02-14 20:59:40 +0100

    addded the drink lists to display whn the page of each race load

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const port = 4400;
 
 app.use(express.static('public'));
 
-app.post('/users', (req,res)=>{
+app.post('/users', (req, res) => {
     console.log(req.query)
     const dataJson = JSON.stringify(req.query);
-    fs.writeFile('user.json',dataJson,(error)=>{
-        if(error){
-        console.log(error);}
-    else{
-        console.log('success');
-    }} )
+    fs.writeFile('user.json', dataJson, (error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log('success');
+        }
+    })
 })
 // app.get('/users/reserve.html', (req,res)=>{
 //     fs.readFile('./public/reserve.html', (error,data)=>{
@@ -27,15 +29,29 @@ app.post('/users', (req,res)=>{
 // })
 app.get('/voidborn', (req, res) => {
     res.sendFile(__dirname + '/public/voidborn.html');
-    });app.get('/human', (req, res) => {
-        res.sendFile(__dirname + '/public/human.html');
-    });app.get('/yordle', (req, res) => {
-        res.sendFile(__dirname + '/public/yordle.html');
-    });app.get('/celestial', (req, res) => {
-        res.sendFile(__dirname + '/public/celestial.html');
-    });
-    
+});
+app.get('/human', (req, res) => {
+    res.sendFile(__dirname + '/public/human.html');
+});
+app.get('/yordle', (req, res) => {
+    res.sendFile(__dirname + '/public/yordle.html');
+});
+app.get('/celestial', (req, res) => {
+    res.sendFile(__dirname + '/public/celestial.html');
+});
+app.get('/drinks',(req,res)=>{
+    fs.readFile('drinks.json', (err,data)=>{
+        if(err){
+         console.log(err.message)
+         return
+        }
+        const drinkList = JSON.parse(data);
+        res.send(drinkList);
+    })
+})
 
-app.listen(port, ()=>{
+
+
+app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 })
